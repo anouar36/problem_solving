11d130899c5b6138e44245be4ecd87cfce242519
anouar <anouarechcharai@gmail.com>
2025-03-17 11:09:48 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// Déclarez une variable nom avec votre nom et affichez-la dans la console avec console.log(nom).
+
+let name = "anouar";
+console.log(name);
+
+// Créez une constante age avec votre âge et affichez-la.
+let a = 10;
+let b = 20;
+console.log(a + b);
+
+// Concaténez deux chaînes de caractères (votre prénom et nom) dans une variable nomComplet et affichez-la
+let firste = "anouar ";
+let laste = "ech-charai";
+console.log(firste + laste);
+
+
+let StingNumber = "10"
+let Number1 = parseInt(StingNumber);
+let Number2 = 12
+console.log();
+
+
